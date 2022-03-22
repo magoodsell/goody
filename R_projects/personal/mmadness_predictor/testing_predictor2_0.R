42308e44a4211fafb17e1dcf2d2ad8950ae8a14a
magoodsell <goo15004@byui.edu>
2022-03-21 20:51:43 -0600

    Cleaned up the script.

--- R_projects/personal/mmadness_predictor/testing_predictor2_0.R
+++ R_projects/personal/mmadness_predictor/testing_predictor2_0.R
@@ -1,19 +1,21 @@
+
+# Loading in the data
 dat <- hoopR::load_mbb_team_box(seasons = 2022)
+
+# getting only the current seasons information
 dat$game_date <- as.Date.character(dat$game_date)
 dat <- dat %>% dplyr::filter(game_date >= '2021-11-09' & 'game_date' <= '2022-03-09')
 
-'S Dakota St', 'South Dakota State'
 
-'Gonzaga'
-
+# the parameters for the function. 
 team_1_test <- 'Jackson State'
 
 team_1_opp_name <- 'Jackson State'
 
+
+# Getting the  data
 team_1_dat <- dat %>% 
   filter(team_short_display_name == team_1_test) %>% 
-  # would need to filter to only 11-09-21 to 03-09-22 to ensure it is only regular season 
-  # OR filter to only season_type == 2?? not sure what 2 means exaclty
   arrange(game_date) %>% 
   select('team_short_display_name'
          , 'team_name'
@@ -44,11 +46,9 @@ team_1_dat <- team_1_dat %>%
   select('school', 'team_name', 'points', 'game_id')
 
 
-# getting gonzaga as the opponents name to get the opponents score
+
 team_1_opponents <- dat %>% 
   filter(opponent_name == team_1_opp_name) %>% 
-  # would need to filter to only 11-09-21 to 03-09-22 to ensure it is only regular season 
-  # OR filter to only season_type == 2?? not sure what 2 means exaclty
   arrange(game_date) %>% 
   select('opponent_name'
          , 'team_short_display_name'
@@ -102,127 +102,35 @@ team_1_final_dat <- team_1_final_dat %>%
 
 View(team_1_final_dat)
 
-## Logistic regression model's
+##########
+# Logistic Regression Model
+
+## Model's
 team_1_lm <- glm(result == 'W' ~ opp_points, data = team_1_final_dat, family = binomial)
 summary(team_1_lm)
 
+## Creating the visual
 beta_1 <- team_1_lm$coefficients[1]
-
 beta_2 <- team_1_lm$coefficients[2]
 
 plot(result=='W' ~ opp_points, data = team_1_final_dat, main=paste(team_1_test, "2022 logistics Regression"))
+curve(exp(beta_1 + beta_2*x)/(1+exp(beta_1 + beta_2*x)), add = TRUE) 
 
 
-
-curve(exp(beta_1 + beta_2*x)/(1+exp(beta_1 + beta_2*x)), add = TRUE) #, from = 0, to = 130)
-
-
-
-# calculating the odds 
+# calculating the odds of the team winning
 ## Getting ppg
 team_1_ppg <- mean(team_1_final_dat$points) 
 
 team_2_ppg <- 86.7
+
 ## Team 1 prediction odds
 team_1_odds <- predict(team_1_lm, data.frame(opp_points=team_2_ppg), type='response')
 
 team_1_odds
 
-####### Testing section 2
-teams_dat <- data.frame(teams = c('Rutgers_ScarletKnights', 'NotreDame_FightingIrish', 'Wyoming_Cowboys', 'Indiana_Hoosiers'
-           , 'WrightState_Raiders',	'Bryant_Bulldogs','TexasSouthern_Tigers',	'TexasA&MCC_Islanders'
-           , 'Gonzaga_Bulldogs',	'GeorgiaState_Panthers','BoiseState_Broncos',	'Memphis_Tigers'
-           ,'Connecticut_Huskies',	'NewMexicoState_Aggies','Arkansas_Razorbacks',	'Vermont_Catamounts'
-           , 'Alabama_CrimsonTide',	'TexasTech_RedRaiders',	'MontanaState_Bobcats',	'MichiganState_Spartans'
-           ,	'Davidson_Wildcats', 'Duke_BlueDevils',	'CalStateFullerton_Titans',	'Baylor_Bears'
-           ,	'NorfolkState_Spartans', 'NorthCarolina_TarHeels',	'Marquette_GoldenEagles', 'SaintMarys_Gaels'
-           ,	'UCLA_Bruins',	'Akron_Zips',	'Texas_Longhorns',	'VirginiaTech_Hokies',	'Purdue_Boilermakers'
-           ,	'Yale_Bulldogs',	'MurrayState_Racers',	'SanFrancisco_Dons',	'Kentucky_Wildcats',	'SaintPeters_Peacocks'
-           ,	'Arizona_Wildcats',	'SetonHall_Pirates',	'TCU_HornedFrogs',	'Houston_Cougars',	'UAB_Blazers'
-           ,	'Illinois_FightingIllini',	'Chattanooga_Mocs',	'ColoradoState_Rams',	'Michigan_Wolverines'
-           ,	'Tennessee_Volunteers',	'Longwood_Lancers',	'OhioState_Buckeyes',	'LoyolaChicago_Ramblers', 'Villanova_Wildcats'
-           ,	'Delaware_FightinBlueHens',	'Kansas_Jayhawks',	'SanDiegoState_Aztecs',	'Creighton_Bluejays','Iowa_Hawkeyes'
-           ,	'Richmond_Spiders',	'Providence_Friars',	'SDakotaState_Jackrabbits',	'LouisianaState_Tigers',	'IowaState_Cyclones'
-           ,'Wisconsin_Badgers',	'Colgate_Raiders',	'SouthernCalifornia_Trojans',	'Miami_Hurricanes'
-           ,	'Auburn_Tigers',	'JacksonState_Tigers'
-))
-
-teams <- list('Rutgers_ScarletKnights', 'NotreDame_FightingIrish', 'Wyoming_Cowboys', 'Indiana_Hoosiers'
-              , 'WrightState_Raiders',	'Bryant_Bulldogs','TexasSouthern_Tigers',	'TexasA&MCC_Islanders'
-              , 'Gonzaga_Bulldogs',	'GeorgiaState_Panthers','BoiseState_Broncos',	'Memphis_Tigers'
-              ,'Connecticut_Huskies',	'NewMexicoState_Aggies','Arkansas_Razorbacks',	'Vermont_Catamounts'
-              , 'Alabama_CrimsonTide',	'TexasTech_RedRaiders',	'MontanaState_Bobcats',	'MichiganState_Spartans'
-              ,	'Davidson_Wildcats', 'Duke_BlueDevils',	'CalStateFullerton_Titans',	'Baylor_Bears'
-              ,	'NorfolkState_Spartans', 'NorthCarolina_TarHeels',	'Marquette_GoldenEagles', 'SaintMarys_Gaels'
-              ,	'UCLA_Bruins',	'Akron_Zips',	'Texas_Longhorns',	'VirginiaTech_Hokies',	'Purdue_Boilermakers'
-              ,	'Yale_Bulldogs',	'MurrayState_Racers',	'SanFrancisco_Dons',	'Kentucky_Wildcats',	'SaintPeters_Peacocks'
-              ,	'Arizona_Wildcats',	'SetonHall_Pirates',	'TCU_HornedFrogs',	'Houston_Cougars',	'UAB_Blazers'
-              ,	'Illinois_FightingIllini',	'Chattanooga_Mocs',	'ColoradoState_Rams',	'Michigan_Wolverines'
-              ,	'Tennessee_Volunteers',	'Longwood_Lancers',	'OhioState_Buckeyes',	'LoyolaChicago_Ramblers', 'Villanova_Wildcats'
-              ,	'Delaware_FightinBlueHens',	'Kansas_Jayhawks',	'SanDiegoState_Aztecs',	'Creighton_Bluejays','Iowa_Hawkeyes'
-              ,	'Richmond_Spiders',	'Providence_Friars',	'SDakotaState_Jackrabbits',	'LouisianaState_Tigers',	'IowaState_Cyclones'
-              ,'Wisconsin_Badgers',	'Colgate_Raiders',	'SouthernCalifornia_Trojans',	'Miami_Hurricanes'
-              ,	'Auburn_Tigers',	'JacksonState_Tigers'
-)
-teams <- str_flatten(teams, collapse = '|')
-team_1_test <- 'Rutgers_ScarletKnights'
-team_1_play <- str_match(teams, team_1_test) 
-team_1_play <- str_split(team_1_play, '_') 
-team_1_play <- team_1_play[[1]]
-team_1_play
-
-
-View(teams_dat)
-teams_dat <- teams_dat %>% 
-  filter(teams == team_1_test) %>% 
-  separate(teams, into = c('school', 'mascot'), sep = '_')
-team_1 <- teams_dat$school[1]  
-teams_dat[1]
-
-
-teams_dat <- data.frame(teams = c('Rutgers_ScarletKnights', 'NotreDame_FightingIrish', 'Wyoming_Cowboys', 'Indiana_Hoosiers'
-                                  , 'WrightState_Raiders',	'Bryant_Bulldogs','TexasSouthern_Tigers',	'TexasA&MCC_Islanders'
-                                  , 'Gonzaga_Bulldogs',	'GeorgiaState_Panthers','BoiseState_Broncos',	'Memphis_Tigers'
-                                  ,'Connecticut_Huskies',	'NewMexicoState_Aggies','Arkansas_Razorbacks',	'Vermont_Catamounts'
-                                  , 'Alabama_CrimsonTide',	'TexasTech_RedRaiders',	'MontanaState_Bobcats',	'MichiganState_Spartans'
-                                  ,	'Davidson_Wildcats', 'Duke_BlueDevils',	'CalStateFullerton_Titans',	'Baylor_Bears'
-                                  ,	'NorfolkState_Spartans', 'NorthCarolina_TarHeels',	'Marquette_GoldenEagles', 'SaintMarys_Gaels'
-                                  ,	'UCLA_Bruins',	'Akron_Zips',	'Texas_Longhorns',	'VirginiaTech_Hokies',	'Purdue_Boilermakers'
-                                  ,	'Yale_Bulldogs',	'MurrayState_Racers',	'SanFrancisco_Dons',	'Kentucky_Wildcats',	'SaintPeters_Peacocks'
-                                  ,	'Arizona_Wildcats',	'SetonHall_Pirates',	'TCU_HornedFrogs',	'Houston_Cougars',	'UAB_Blazers'
-                                  ,	'Illinois_FightingIllini',	'Chattanooga_Mocs',	'ColoradoState_Rams',	'Michigan_Wolverines'
-                                  ,	'Tennessee_Volunteers',	'Longwood_Lancers',	'OhioState_Buckeyes',	'LoyolaChicago_Ramblers', 'Villanova_Wildcats'
-                                  ,	'Delaware_FightinBlueHens',	'Kansas_Jayhawks',	'SanDiegoState_Aztecs',	'Creighton_Bluejays','Iowa_Hawkeyes'
-                                  ,	'Richmond_Spiders',	'Providence_Friars',	'SDakotaState_Jackrabbits',	'LouisianaState_Tigers',	'IowaState_Cyclones'
-                                  ,'Wisconsin_Badgers',	'Colgate_Raiders',	'SouthernCalifornia_Trojans',	'Miami_Hurricanes'
-                                  ,	'Auburn_Tigers',	'JacksonState_Tigers'
-)) %>% 
-  mutate(teams_x = teams) %>% 
-  separate(teams, into = c('school', 'mascot'), sep = '_') 
-  # will need to add space when there is a capital letter in name
-  # teams_dat <- teams_dat %>% str_replace(teams_dat$school,'TexasA&mcc', 'Texas A&M-CC')
-  teams_dat['school'][teams_dat['school'] == "TexasA&MCC"] <- 'Texas A&M-CC'
-  teams_dat['school'][teams_dat['school'] == "SDakotaState"] <- 'S Dakota State'
-  teams_dat$school <- gsub("([a-z])([A-Z])","\\1 \\2",teams_dat$school)
-  teams_dat['school'][teams_dat['school'] == "Connecticut"] <- 'UConn'
-  teams_dat['school'][teams_dat['school'] == "Jackson State"] <- "J'Ville St"
-  teams_dat['school'][teams_dat['school'] == "Southern California"] <- 'USC'
-  
-  
-  
-### getting team 1
-team_1 <- teams_dat %>% 
-  filter(teams == team_1) %>% 
-  teams_dat$school[1]
-
-### Getting team 2
-team_2 <- teams_dat %>% 
-  filter(teams == team_2) %>% 
-  teams_dat$school[1]
-
-
-
-#############3
+#############
+#Testing section 2
+#############
 
 team_1= 'Rutgers_ScarletKnights'
 team_2 = 'Bryant_Bulldogs'
@@ -246,18 +154,17 @@ teams_dat <- data.frame(teams = c('Rutgers_ScarletKnights', 'NotreDame_FightingI
 )) %>% 
   mutate(teams_x = teams) %>% 
   separate(teams, into = c('school', 'mascot'), sep = '_') 
-# will need to add space when there is a capital letter in name
-# teams_dat <- teams_dat %>% str_replace(teams_dat$school,'TexasA&mcc', 'Texas A&M-CC')
+
+
 teams_dat['school'][teams_dat['school'] == "TexasA&MCC"] <- 'Texas A&M-CC'
 teams_dat['school'][teams_dat['school'] == "SDakotaState"] <- 'S Dakota State'
 teams_dat$school <- gsub("([a-z])([A-Z])","\\1 \\2",teams_dat$school)
 teams_dat['school'][teams_dat['school'] == "Connecticut"] <- 'UConn'
-# teams_dat['school'][teams_dat['school'] == "Jackson State"] <- "J'Ville St" # ? I think Jackson State is good
 teams_dat['school'][teams_dat['school'] == "Southern California"] <- 'USC'
 teams_dat['school'][teams_dat['school'] == "Saint Peters"] <- "Saint Peter's"
 teams_dat['school'][teams_dat['school'] == "New Mexico State"] <- "New Mexico St"
 teams_dat['school'][teams_dat['school'] == "Saint Marys"] <- "Saint Mary's"
-
+# teams_dat['school'][teams_dat['school'] == "Jackson State"] <- "J'Ville St" # ? I think Jackson State is good
 
 
 ### getting team 1
