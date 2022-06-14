73a94b816c5e218e8449bef078aa503dff7a6ccc
magoodsell <goo15004@byui.edu>
2022-06-14 14:47:05 -0600

    Adding R script for my timeline idea of D & C idea. Contains other ideas.

--- R_projects/Church_Related/DocandCov.R
+++ R_projects/Church_Related/DocandCov.R
@@ -1,5 +1,5 @@
 ########
-# Creating a timeline of the Doctring and Covenants
+# Creating a timeline of the Doctrine and Covenants and exploring other visuals
 ########
 
 # loading packages needed
@@ -14,6 +14,27 @@ vistime(docandcov, col.event = "Year", col.group = "Section Number",
         col.start = "Year", col.end = "Year" , title = "Chronology of Sections")
 # getting error - think in order to use this package I will need to add in columns. 
 
+
+# making Month a factor
+docandcov$Month <- factor(docandcov$Month, levels = c('January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December', 'Unknown'))
+
+# visualizing by the count of revelations by month
 docandcov %>% 
   ggplot(aes(Month)) +
-  geom_bar()
+  geom_bar() + 
+  geom_text(stat='count', aes(label=..count..), vjust=-0.5) +
+  theme(axis.text.x = element_text(angle=90))
+
+
+# visualizing the count of revelations by state
+docandcov %>% 
+  ggplot(aes(x = fct_infreq(State))) +
+  geom_bar() +  
+  geom_text(stat='count', aes(label=..count..), vjust=-0.2) +
+  theme_minimal() + 
+  theme(axis.text.x = element_text(angle=90)) + 
+  labs(title = 'Revelation Count by State', x = 'State', y = '')
+
+# take above visual and put it a table  
+  
+  
